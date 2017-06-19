84962baa76c90aa1f842ba744837067eada8f692
Fernandez, Fernnando <fernando.fernandez@valantmed.com>
2017-06-19 13:23:44 +0200

    Added some use cases

---
+++ src/S3.TestServer/S3.TestServer/IHttpTestServer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace S3.TestServer
+{
+    public interface IHttpTestServer
+    {
+        Uri HostUrl { get; }
+        int Port { get; }
+        void Start();
+        void Stop();
+    }
+}
