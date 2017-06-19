bb3677cb9f7ce52e6dfb1a0064be88f6337f0653
Fernandez, Fernnando <fernando.fernandez@valantmed.com>
2017-06-19 13:23:26 +0200

    Added some use cases

--- src/S3.TestServer/S3.TestServer.Test/ServerTest.cs
+++ src/S3.TestServer/S3.TestServer.Test/ServerTest.cs
@@ -7,11 +7,20 @@ namespace S3.TestServer.Test
     public class ServerTest
     {
         [Fact]
-        public void return_httptestserver_instance()
+        public void Create_Httptestserver_WithNull_Parameter_Return_ArgumentNullException()
+        {
+            Action HttpServerAction = () => new HttpTestServer(null);
+
+            AssertExtensions.ShouldThrow<ArgumentNullException>(HttpServerAction);
+        }
+
+        [Fact]
+        public void Create_Httptestserver_Return_Instance()
         {
             var httpServer = new HttpTestServer();
 
             httpServer.ShouldNotBeNull();
+            httpServer.ShouldBeOfType<HttpTestServer>();
         }
     }
 }
--- src/S3.TestServer/S3.TestServer/HttpTestServer.cs
+++ src/S3.TestServer/S3.TestServer/HttpTestServer.cs
@@ -1,12 +1,41 @@
-﻿using System.Web.Http;
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using CuttingEdge.Conditions;
 
 namespace S3.TestServer
 {
-    public class HttpTestServer
+    public class HttpTestServer: IHttpTestServer
     {
-        public HttpTestServer()
+        public Uri HostUrl { get; private set; }
+        public int Port { get; private set; }
+
+        private readonly HttpListener httpListener;
+
+        public HttpTestServer(HttpListener httpListener)
         {
-            
+            Condition.Requires(httpListener, nameof(httpListener)).IsNotNull();
+            this.httpListener = httpListener;
+            SetLocalhostAddressAndPort();
+            httpListener.Prefixes.Add(this.HostUrl.ToString());
+        }
+
+        public HttpTestServer() : this(new HttpListener())
+        {
+        }
+
+        public void Start() => this.httpListener.Start();
+
+        public void Stop() => this.httpListener.Stop();
+
+        private void SetLocalhostAddressAndPort()
+        {
+            var listener = new TcpListener(IPAddress.Loopback, 0);
+            listener.Start();
+            this.Port = ((IPEndPoint) listener.LocalEndpoint).Port;
+            listener.Stop();
+
+            this.HostUrl = new Uri($"http://localhost:{this.Port}/");
         }
     }
 }
